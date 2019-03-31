071c6ec4d81a31d95a4ca597a8a8dfc04585337b
N9-1 <49112859+N9-1@users.noreply.github.com>
2019-03-31 14:44:25 +0700

    Create Exercise4_Ratchanon_B.py

---
+++ assignments/Exercise4_Ratchanon_B.py
@@ -0,0 +1,9 @@
+Feng = 100
+Glb = 98
+Intcps = 95.5
+Cpp = 90
+print("---","Your Score","---")
+print("Foundation English               :", Feng)
+print("General Business                 :", Glb)
+print("Introduction to Computer Systems :", Intcps)
+print("Computer Programming             :", Cpp)
