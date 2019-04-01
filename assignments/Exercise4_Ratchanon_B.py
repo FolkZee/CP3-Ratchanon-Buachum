96af10af80a8839427d6ca2c1617ec0707b59924
N9-1 <49112859+N9-1@users.noreply.github.com>
2019-04-01 10:18:41 +0700

    u

--- assignments/Exercise4_Ratchanon_B.py
+++ assignments/Exercise4_Ratchanon_B.py
@@ -2,7 +2,7 @@ Feng = 100
 Glb = 98
 Intcps = 95.5
 Cpp = 90
-print("---","Your Score","---")
+print("---","Your Score","---\n")
 print("Foundation English               :", Feng)
 print("General Business                 :", Glb)
 print("Introduction to Computer Systems :", Intcps)
