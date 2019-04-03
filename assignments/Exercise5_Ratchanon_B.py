8cde86f0554ccdd549d04515bff29777e1534c15
N9-1 <49112859+N9-1@users.noreply.github.com>
2019-04-03 20:20:24 +0700

    1

---
+++ assignments/Exercise5_Ratchanon_B.py
@@ -0,0 +1,5 @@
+Num     = int(input("number : "))
+Num2    = int(input("number : "))
+
+
+print(Num,"+",Num2,"=",(Num+Num2),"\n"+str(Num),"-",Num2,"=",(Num-Num2),"\n"+str(Num),"*",Num2,"=",(Num*Num2),"\n"+str(Num),"/",Num2,"=",float(Num/Num2))
