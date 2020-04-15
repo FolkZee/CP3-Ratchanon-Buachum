b81fcfa6d75faa723229c264ecee28dc26b66e84
N9-1 <49112859+N9-1@users.noreply.github.com>
2020-04-15 21:22:21 +0700

    ++

--- assignments/Exercise5_Ratchanon_B.py
+++ assignments/Exercise5_Ratchanon_B.py
@@ -2,4 +2,4 @@ Num     = int(input("number : "))
 Num2    = int(input("number : "))
 
 
-print(Num,"+",Num2,"=",(Num+Num2),"\n"+str(Num),"-",Num2,"=",(Num-Num2),"\n"+str(Num),"*",Num2,"=",(Num*Num2),"\n"+str(Num),"/",Num2,"=",(Num/Num2))
+print(Num,"+",Num2,"=",(Num+Num2),"\n"+str(Num),"-",Num2,"=",(Num-Num2),"\n"+str(Num),"*",Num2,"=",(Num*Num2),"\n"+str(Num),"/",Num2,"=",int(Num/Num2))
