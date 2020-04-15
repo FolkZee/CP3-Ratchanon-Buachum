b3b306149c05c364678894f2aaebf9449a055620
N9-1 <49112859+N9-1@users.noreply.github.com>
2020-04-15 21:35:10 +0700

    Update Exercise4_Ratchanon_B.py

--- assignments/Exercise4_Ratchanon_B.py
+++ assignments/Exercise4_Ratchanon_B.py
@@ -1,7 +1,7 @@
-Feng = 100
-Glb = 98
-Intcps = 95.5
-Cpp = 90
+Feng    = 100
+Glb     = 98
+Intcps  = 95.5
+Cpp     = 90
 print("---","Your Score","---\n")
 print("Foundation English               :", Feng)
 print("General Business                 :", Glb)
