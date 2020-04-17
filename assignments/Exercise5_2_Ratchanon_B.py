dc58372578e12173de8a505d0bad6e5ba8c1e629
N9-1 <49112859+N9-1@users.noreply.github.com>
2020-04-17 14:45:30 +0700

    ++

--- assignments/Exercise5_2_Ratchanon_B.py
+++ assignments/Exercise5_2_Ratchanon_B.py
@@ -1,6 +1,5 @@
 print("-"*5,"velocity calculator","-"*5)
 s = int(input("distance     : "))
 t = int(input("time         : "))
-unit = input("m/s or km/h  : ")
 v = s/t
-print(v,unit)
+print(v,"km/h")
